043e0b580c03359c91f493c0a1098eb0e6367851
Charles Sena <charlessena@gokeipsti.com.br>
2024-05-06 14:12:08 -0300

    Time argument feature

--- main.py
+++ main.py
@@ -1,10 +1,21 @@
-#!/bin/python
+#!/usr/bin/env python
 
+import argparse
 import pyautogui
-import time
+from datetime import datetime
+from time import sleep
 
-time.sleep(3)
+parser = argparse.ArgumentParser()
+parser.add_argument('--time',required = True)
+args = parser.parse_args()
+
+format = '%H:%M:%S'
+
+while datetime.now().strftime(format) != args.time:
+    print(datetime.now().strftime(format))
+    print(args.time)
+    sleep(0.5)
+    pass
 
 pyautogui.click()
-
 
